40dfe55cd1257223b04ccd25df6b3ebfdb99ee74
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-05-11 14:38:37 -0300

    Add event listener to button and creteNewBook function

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 
 const submit = document.getElementById("submit");
 
-const title = document.getElementById("title").value,
-  author = document.getElementById("author").value,
-  pages = document.getElementById("pages").value;
+
 
 let myLibrary = [];
 
@@ -15,6 +13,21 @@ function newBook(title, author, pages, read = false) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+submit.addEventListener("click", (e) => createNewBook(e));
+
+function createNewBook(e) {
+  const title = document.getElementById("title").value,
+      author = document.getElementById("author").value,
+      pages = document.getElementById("pages").value;
+
+  e.preventDefault();
+  const aNewBook = new newBook(title, author, pages);
+  addBookToLibrary(aNewBook);
+  const form = document.getElementById("form");
+  form.reset();
 }
+
+function addBookToLibrary(aNewBook) {
+  myLibrary.push(aNewBook);
+}
+
